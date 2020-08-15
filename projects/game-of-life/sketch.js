a3a8efc5a3e38552ff5b3f8345a4015dc134c1dc
Jordan Millett <JelloJordan3@gmail.com>
2020-08-14 21:08:37 -0700

    altering file structure

---
+++ projects/game-of-life/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  fill(25);
+  ellipse(width/2, height/2, 300 , 300);
+  fill(255);
+  textSize(50);
+  text('Hello World', 75, height/2);
+}
